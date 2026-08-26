7ed5eb28d901eb6603aef430363d195fef458f5c
Zair Chavarin <a225209578@unison.mx>
2026-08-25 23:36:02 -0700

    test: adicion de pruebas unitarias para clientes premium (Fase Red)

--- iteracion-xp/src/CalculadorEnvio.java
+++ iteracion-xp/src/CalculadorEnvio.java
@@ -1,19 +1,6 @@
 public class CalculadorEnvio {
 
-    private static final double LIMITE_ENVIO_GRATIS = 1000.0;
-    private static final double LIMITE_ENVIO_CONCESION = 500.0;
-
-    private static final double COSTO_ESTANDAR = 100.0;
-    private static final double COSTO_CONCESION = 50.0;
-    private static final double COSTO_GRATIS = 0.0;
-
-    public static double calcularCostoEnvio(double importePedido) {
-        if (importePedido >= LIMITE_ENVIO_GRATIS) {
-            return COSTO_GRATIS;
-        }
-        if (importePedido >= LIMITE_ENVIO_CONCESION) {
-            return COSTO_CONCESION;
-        }
-        return COSTO_ESTANDAR;
+    public static double calcularCostoEnvio(double importePedido, boolean esPremium) {
+        return -1.0;
     }
 }
--- iteracion-xp/src/CalculadorEnvioTest.java
+++ iteracion-xp/src/CalculadorEnvioTest.java
@@ -4,14 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculadorEnvioTest {
 
     @Test
-    void testCalcularCostoEnvio() {
-        assertEquals(100.0, CalculadorEnvio.calcularCostoEnvio(400.0));
-        assertEquals(50.0, CalculadorEnvio.calcularCostoEnvio(700.0));
-        assertEquals(0.0, CalculadorEnvio.calcularCostoEnvio(1200.0));
+    void testCalcularCostoEnvioEstandar() {
+        assertEquals(100.0, CalculadorEnvio.calcularCostoEnvio(400.0, false));
+        assertEquals(50.0, CalculadorEnvio.calcularCostoEnvio(700.0, false));
+        assertEquals(0.0, CalculadorEnvio.calcularCostoEnvio(1200.0, false));
 
-        assertEquals(100.0, CalculadorEnvio.calcularCostoEnvio(499.99));
-        assertEquals(50.0, CalculadorEnvio.calcularCostoEnvio(500.00));
-        assertEquals(50.0, CalculadorEnvio.calcularCostoEnvio(999.99));
-        assertEquals(0.0, CalculadorEnvio.calcularCostoEnvio(1000.00));
+        assertEquals(100.0, CalculadorEnvio.calcularCostoEnvio(499.99, false));
+        assertEquals(50.0, CalculadorEnvio.calcularCostoEnvio(500.00, false));
+        assertEquals(50.0, CalculadorEnvio.calcularCostoEnvio(999.99, false));
+        assertEquals(0.0, CalculadorEnvio.calcularCostoEnvio(1000.00, false));
+    }
+
+    @Test
+    void testCalcularCostoEnvioPremium() {
+        assertEquals(0.0, CalculadorEnvio.calcularCostoEnvio(100.0, true));
+        assertEquals(0.0, CalculadorEnvio.calcularCostoEnvio(499.99, true));
     }
 }
