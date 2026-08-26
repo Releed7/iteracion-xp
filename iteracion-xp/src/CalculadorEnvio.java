8c500fddc7369c721a4191204f6fe5236149b250
Zair Chavarin <a225209578@unison.mx>
2026-08-25 23:37:54 -0700

    feat: soporte para envio gratuito a clientes premium en verde (Fase Green)

--- iteracion-xp/src/CalculadorEnvio.java
+++ iteracion-xp/src/CalculadorEnvio.java
@@ -1,6 +1,19 @@
 public class CalculadorEnvio {
 
+    private static final double LIMITE_ENVIO_GRATIS = 1000.0;
+    private static final double LIMITE_ENVIO_CONCESION = 500.0;
+
+    private static final double COSTO_ESTANDAR = 100.0;
+    private static final double COSTO_CONCESION = 50.0;
+    private static final double COSTO_GRATIS = 0.0;
+
     public static double calcularCostoEnvio(double importePedido, boolean esPremium) {
-        return -1.0;
+        if (esPremium || importePedido >= LIMITE_ENVIO_GRATIS) {
+            return COSTO_GRATIS;
+        }
+        if (importePedido >= LIMITE_ENVIO_CONCESION) {
+            return COSTO_CONCESION;
+        }
+        return COSTO_ESTANDAR;
     }
 }
