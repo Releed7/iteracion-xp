4de895511cf713ca8960097402dfae061690ae9c
Zair Chavarin <a225209578@unison.mx>
2026-08-25 23:29:51 -0700

    feat: implementación minima para pasar las pruebas en verde (Fase Green)

--- iteracion-xp/src/CalculadorEnvio.java
+++ iteracion-xp/src/CalculadorEnvio.java
@@ -1,6 +1,12 @@
 public class CalculadorEnvio {
 
     public static double calcularCostoEnvio(double importePedido) {
-        return -1.0;
+        if (importePedido >= 1000.0) {
+            return 0.0;
+        }
+        if (importePedido >= 500.0) {
+            return 50.0;
+        }
+        return 100.0;
     }
 }
